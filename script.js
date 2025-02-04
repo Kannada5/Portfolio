ddec2f5dd51fe17c4d31db66485880e41aac4e94
Gowtham Acharya <97829249+Kannada5@users.noreply.github.com>
2025-02-04 21:37:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        const portfolioTabs = document.querySelectorAll('#portfolio-tabs .nav-link');
+        const portfolioContent = document.querySelectorAll('.tab-pane');
+
+        portfolioTabs.forEach(tab => {
+            tab.addEventListener('click', () => {
+                const target = tab.getAttribute('data-bs-target');
+                portfolioContent.forEach(content => {
+                    content.classList.remove('show', 'active');
+                });
+                document.querySelector(target).classList.add('show', 'active');
+            });
+        });
+    });
+</script>
 
